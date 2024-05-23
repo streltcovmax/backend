70bb8a20daa7ad87cfbc88cebb3188bff113366c
streltcovmax <144123001+streltcovmax@users.noreply.github.com>
2024-05-23 23:52:23 +0300

    Add files via upload

--- task5/login.php
+++ task5/login.php
@@ -1,32 +1,43 @@
 <?php
-
-    // Подключаем файл с настройками базы данных
+    session_start();
     include 'db_credentials.php';
-    try {
-        $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD,
-            [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    } catch (PDOException $e) {
-        echo 'Подключение не удалось: ' . $e->getMessage();
-        exit;
-    }
+    
     // Проверяем, если форма была отправлена
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Получаем введенные пользователем логин и пароль
-        $login = $_POST['login'];
-        $password = $_POST['password'];
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") 
+    {
+        if(isset($_POST['enterAcc'])) 
+        {
+            // Получаем введенные пользователем логин и пароль
+            $login = $_POST['login'];
+            $password = $_POST['password'];
 
-        // Выполняем запрос к базе данных, чтобы найти пользователя с указанным логином
-        $stmt = $db->prepare("SELECT * FROM Users WHERE username = ?");
-        $stmt->execute([$login]);
-        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            // Выполняем запрос к базе данных, чтобы найти пользователя с указанным логином
+            $stmt = $db->prepare("SELECT * FROM Users WHERE username = ?");
+            $stmt->execute([$login]);
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        // Если пользователь найден и хеш пароля совпадает, устанавливаем сессию для него
-        if ($user && password_verify($password, $user['password'])) {
-            $_SESSION['username'] = $login;
-            header("Location: form.php"); 
-            exit();
-        } else {
-            header("Location: index.php"); 
+            // Если пользователь найден и хеш пароля совпадает, устанавливаем сессию для него
+            if ($user && password_verify($password, $user['password'])) 
+            {
+                $_SESSION['username'] = $login;
+                header("Location: form.php"); 
+                exit();
+            } 
+            else 
+            {
+                // Пользователь не найден
+                //header("Location: index.php");
+                echo "<p style='color:red;'>", "Неправильный логин или пароль!";
+                exit();
+            }
+        }
+        elseif (isset($_POST['createAcc'])) 
+        {
+            session_destroy();
+            setcookie('username', '', time()-3600);
+            header("Location: form.php");
             exit();
         }
     }
+?>
